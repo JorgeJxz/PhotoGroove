c6ce5f031ed1566dd876069ac124aa9e88af4491
Jorge Andres Gonzalez De Freitas <joseandres.gonzalez.freire.contractor@bbva.com>
2023-12-15 22:59:40 +0100

    cambios cap 2 pag 38

--- src/PhotoGroove.elm
+++ src/PhotoGroove.elm
@@ -1,17 +1,23 @@
 module PhotoGroove exposing (main)
 
-import Html exposing (div,h1,img,text)
+import Html exposing (..)
 import Html.Attributes exposing (..)
+
+urlPrefix =
+    "https://picsum.photos/200/300?random="
 
 view model =
     div [ class "content" ]
         [ h1 [] [ text "Photo Groove" ]
-        , div [id "thumbnails" ]
-            [ img [ src "https://picsum.photos/200/300?random=1"] []
-            , img [ src "https://picsum.photos/200/300?random=2"] []
-            , img [ src "https://picsum.photos/200/300?random=3"] []
-            ]
+        , div [ id "thumbnails" ] []
         ]
+viewThumbnail thumb =
+    img [ src (urlPrefix ++ thumb.url) ] []
+initialModel =
+    [ { url = "1" }
+    , { url = "2" }
+    , { url = "3" }
+    ]
 
 main  =
-    view "no model yet"
+    view initialModel
